927b41e922da086124245a4f8b9f4714d13d5063
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-12-09 15:53:05 -0300

    começando requisito 7

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ for (let i = 0; i < paletaCores.length; i += 1) {
     paletaCores[i].style.backgroundColor = cores[i];
 }
 
+
 let getpixelBord = document.getElementById('pixel-board');
-
-
 
 for (let i = 0; i < 25; i += 1) {
     let createPixel = document.createElement('div');
     createPixel.className = 'pixel'
     getpixelBord.appendChild(createPixel);
+}
+
+window.onload = function() {
+    paletaCores[0].className = 'color selected';
+}
+
+function mudaSelectCor() {
+    for (let i = 0; i < paletaCores.length; i += 1){
+        if (paletaCores[i].className === 'color selected') {
+            paletaCores[i].className = 'color';
+        }
+    }
+}
+
+for (let i = 0; i < paletaCores.length; i += 1) {
+    paletaCores[i].addEventListener('click', mudaSelectCor); 
 }
