6189087802b0c1bec87981a63add95e03252aadf
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-01-03 18:17:10 -0300

    melhorando identacao e codigo

--- script.js
+++ script.js
@@ -18,51 +18,51 @@ window.onload = function () {
 };
 
 paletaCores[0].addEventListener('click', function () {
-    for (i = 0; i < paletaCores.length; i += 1){
-        paletaCores[i].className = 'color';
-    }
-    if (paletaCores[0].className !== 'color selected') {
-        paletaCores[0].classList.add('selected');
-    }
+  for (i = 0; i < paletaCores.length; i += 1){
+    paletaCores[i].className = 'color';
+  }
+  if (paletaCores[0].className !== 'color selected') {
+    paletaCores[0].classList.add('selected');
+  }
 });
 
 paletaCores[1].addEventListener('click', function () {
-    for (i = 0; i < paletaCores.length; i += 1){
-        paletaCores[i].className = 'color';
-    }
-    if (paletaCores[1].className !== 'color selected') {
-        paletaCores[1].classList.add('selected');
-    }
+  for (i = 0; i < paletaCores.length; i += 1){
+    paletaCores[i].className = 'color';
+  }
+  if (paletaCores[1].className !== 'color selected') {
+    paletaCores[1].classList.add('selected');
+  }
 });
 
 paletaCores[2].addEventListener('click', function () {
-    for (i = 0; i < paletaCores.length; i += 1){
-        paletaCores[i].className = 'color';
-    }
-    if (paletaCores[2].className !== 'color selected') {
-        paletaCores[2].classList.add('selected');
-    }
+  for (i = 0; i < paletaCores.length; i += 1){
+    paletaCores[i].className = 'color';
+  }
+  if (paletaCores[2].className !== 'color selected') {
+    paletaCores[2].classList.add('selected');
+  }
 });
 
 paletaCores[3].addEventListener('click', function () {
-    for (i = 0; i < paletaCores.length; i += 1){
-        paletaCores[i].className = 'color';
-    }
-    if (paletaCores[3].className !== 'color selected') {
-        paletaCores[3].classList.add('selected');
-    }
+  for (i = 0; i < paletaCores.length; i += 1){
+    paletaCores[i].className = 'color';
+  }
+  if (paletaCores[3].className !== 'color selected') {
+    paletaCores[3].classList.add('selected');
+  }
 });
 
-let selecBody = document.getElementsByTagName('body');
-let buttonClear = document.createElement('button');
+const selecBody = document.getElementsByTagName('body');
+const buttonClear = document.createElement('button');
 buttonClear.innerText = 'Limpar';
 buttonClear.id = 'clear-board';
 selecBody[0].appendChild(buttonClear);
 
-function clearColor () {
-    for(i = 0; i < getPixel.length; i += 1){
-        getPixel[i].style.backgroundColor = 'white';
-    }
+function clearColor() {
+  for (i = 0; i < getPixel.length; i += 1) {
+    getPixel[i].style.backgroundColor = 'white';
+  }
 }
 
 buttonClear.addEventListener('click', clearColor);
@@ -70,14 +70,14 @@ buttonClear.addEventListener('click', clearColor);
 const getPixel = document.getElementsByClassName('pixel');
 const getSelectColor = document.getElementsByClassName('selected');
 
-for (i = 0; i < getPixel.length; i += 1){
-    getPixel[i].addEventListener('click', colorPixel)
+for (i = 0; i < getPixel.length; i += 1) {
+  getPixel[i].addEventListener('click', colorPixel);
 }
 
-function colorPixel (event) {
-    for(i = 0; i < getPixel.length; i += 1){
-        event.target.style.backgroundColor = getSelectColor[0].style.backgroundColor;
-    }
+function colorPixel(event) {
+  for(i = 0; i < getPixel.length; i += 1){
+    event.target.style.backgroundColor = getSelectColor[0].style.backgroundColor;
+  }
 }
 
 const createBoard = document.createElement('input');
@@ -93,22 +93,22 @@ selecBody[0].appendChild(createButtonInput);
 
 createButtonInput.addEventListener('click', newBoard);
 
-function newBoard () {
-    for (i = getPixel.length - 1; i >= 0; i -= 1){
-        getPixel[i].remove();
-    }
-    if (createBoard.value > 50){
-        createBoard.value = 50;
-    }
-    if (createBoard.value < 5){
-        createBoard.value = 5;
-    }
-    for (i = 0; i < createBoard.value*createBoard.value; i += 1) {
-        const createPixel = document.createElement('div');
-        createPixel.className = 'pixel';
-        getPixelBord.appendChild(createPixel);
-    }
-    for (i = 0; i < getPixel.length; i += 1){
-        getPixel[i].addEventListener('click', colorPixel)
-    }
+function newBoard() {
+  for (i = getPixel.length - 1; i >= 0; i -= 1) {
+    getPixel[i].remove();
+  }
+  if (createBoard.value > 50) {
+    createBoard.value = 50;
+  }
+  if (createBoard.value < 5) {
+    createBoard.value = 5;
+  }
+  for (i = 0; i < createBoard.value * createBoard.value; i += 1) {
+    const createPixel = document.createElement('div');
+    createPixel.className = 'pixel';
+    getPixelBord.appendChild(createPixel);
+  }
+  for (i = 0; i < getPixel.length; i += 1) {
+    getPixel[i].addEventListener('click', colorPixel);
+  }
 }
