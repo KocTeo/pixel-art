0acde23349746edb8e7483c05ccd5ac2fd3a51cb
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-01-03 16:33:33 -0300

    requisitos 9, 10 e 11

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ for (i = 0; i < 25; i += 1) {
   createPixel.className = 'pixel';
   getPixelBord.appendChild(createPixel);
 }
-
 window.onload = function () {
   paletaCores[0].className = 'color selected';
 };
@@ -71,78 +70,45 @@ buttonClear.addEventListener('click', clearColor);
 const getPixel = document.getElementsByClassName('pixel');
 const getSelectColor = document.getElementsByClassName('selected');
 
-getPixel[0].addEventListener('click', function () {
-    getPixel[0].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[1].addEventListener('click', function () {
-    getPixel[1].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[2].addEventListener('click', function () {
-    getPixel[2].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[3].addEventListener('click', function () {
-    getPixel[3].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[4].addEventListener('click', function () {
-    getPixel[4].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[5].addEventListener('click', function () {
-    getPixel[5].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[6].addEventListener('click', function () {
-    getPixel[6].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[7].addEventListener('click', function () {
-    getPixel[7].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[8].addEventListener('click', function () {
-    getPixel[8].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[9].addEventListener('click', function () {
-    getPixel[9].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[10].addEventListener('click', function () {
-    getPixel[10].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[11].addEventListener('click', function () {
-    getPixel[11].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[12].addEventListener('click', function () {
-    getPixel[12].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[13].addEventListener('click', function () {
-    getPixel[13].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[14].addEventListener('click', function () {
-    getPixel[14].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[15].addEventListener('click', function () {
-    getPixel[15].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[16].addEventListener('click', function () {
-    getPixel[16].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[17].addEventListener('click', function () {
-    getPixel[17].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[18].addEventListener('click', function () {
-    getPixel[18].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[19].addEventListener('click', function () {
-    getPixel[19].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[20].addEventListener('click', function () {
-    getPixel[20].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[21].addEventListener('click', function () {
-    getPixel[21].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[22].addEventListener('click', function () {
-    getPixel[22].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[23].addEventListener('click', function () {
-    getPixel[23].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
-getPixel[24].addEventListener('click', function () {
-    getPixel[24].style.backgroundColor = getSelectColor[0].style.backgroundColor;
-});
+for (i = 0; i < getPixel.length; i += 1){
+    getPixel[i].addEventListener('click', colorPixel)
+}
+
+function colorPixel (event) {
+    for(i = 0; i < getPixel.length; i += 1){
+        event.target.style.backgroundColor = getSelectColor[0].style.backgroundColor;
+    }
+}
+
+const createBoard = document.createElement('input');
+createBoard.id = 'board-size';
+selecBody[0].appendChild(createBoard);
+createBoard.type = 'number';
+createBoard.min = 1;
+
+const createButtonInput = document.createElement('button');
+createButtonInput.id = 'generate-board';
+createButtonInput.innerText = 'VQV';
+selecBody[0].appendChild(createButtonInput);
+
+createButtonInput.addEventListener('click', newBoard);
+
+function newBoard () {
+    for (i = getPixel.length - 1; i >= 0; i -= 1){
+        getPixel[i].remove();
+    }
+    if (createBoard.value > 50){
+        createBoard.value = 50;
+    }
+    if (createBoard.value < 5){
+        createBoard.value = 5;
+    }
+    for (i = 0; i < createBoard.value*createBoard.value; i += 1) {
+        const createPixel = document.createElement('div');
+        createPixel.className = 'pixel';
+        getPixelBord.appendChild(createPixel);
+    }
+    for (i = 0; i < getPixel.length; i += 1){
+        getPixel[i].addEventListener('click', colorPixel)
+    }
+}
